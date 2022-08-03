24e7adbb272ab7b005c807f5c7f7f10afeca2a92
mirkan-k <mirkilinc@gmail.com>
2022-08-03 19:41:12 +0100

    API working with contacts - Base Criteria Done

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express")
 const morgan = require("morgan")
 const cors = require("cors")
-const contacts = require("./data/contacts.js")
+let contacts = require("./data/contacts.js")
 
 const app = express()
 
@@ -19,7 +19,7 @@ app.get("/", (req, res) => {
 app.get("/contacts", (req, res) => {
     console.log("got request for '/contacts'")
     res.send({
-        contacts: contacts
+        "contacts": contacts
     })
 })
 
@@ -27,8 +27,51 @@ app.get("/contacts", (req, res) => {
 app.get("/contacts/:id", (req, res) => {
     const reqID = req.params.id
     console.log(`got request for '/contacts/${reqID}'`)
-    const contact = contacts.filter(contact => Number(contact.id) === Number(reqID))
-    res.send(contact)
+    const contact = contacts.find(person => Number(person.id) === Number(reqID))
+    res.send({
+        "contact": contact
+    })
+})
+
+// post new contact
+app.post("/contacts", (req, res) => {
+    const newContactInfo = req.body;
+    const newContact = {
+        "id": contacts.length + 1,
+        ...newContactInfo,
+        "meetings": []
+    }
+    contacts.push(newContact)
+    res.send({
+        "contact": newContact
+    })
+})
+
+// delete contact via id
+app.delete("/contacts/:id", (req, res) => {
+    const reqID = req.params.id
+    const newContacts = contacts.filter(contact => Number(contact.id) !== Number(reqID))
+    contacts = newContacts
+    res.status(200).send('OK');
+})
+
+// put, update contact via id
+app.put("/contacts/:id", (req, res) => {
+    const reqID = req.params.id
+    const newContactInfo = req.body
+    const contact = contacts.find(person => Number(person.id) === Number(reqID))
+    const updatedContact = {
+        "id": Number(contact.id),
+        ...newContactInfo
+    }
+    contacts = contacts.map(person => {
+        if (person === contact) {
+           return (updatedContact)
+        } else return (person)
+    })
+    res.send({
+        "contact": updatedContact
+    })
 })
 
 // booting up server:
